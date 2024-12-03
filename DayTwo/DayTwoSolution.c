a10591b87e47a4ea8d94fa5a287c81b25eb1219c
Matthew <matthewkaltman@gmail.com>
2024-12-02 22:53:03 -0800

    basic structure for reading done, day 2

--- DayTwo/DayTwoSolution.c
+++ DayTwo/DayTwoSolution.c
@@ -1,1 +1,18 @@
-c o d e
+#include <stdlib.h>
+#include <stdio.h>
+
+FILE *Fptr;
+
+
+
+int main(void){
+
+    Fptr = fopen("InputData.txt","r");
+    if(Fptr == NULL)
+    {
+        printf("This file doesnt exist\n");
+        return 0;
+    }
+    
+    return 1;
+}
